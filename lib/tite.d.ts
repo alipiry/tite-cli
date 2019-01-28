614e07cbaef2e9a1729d7a33d12fef1240ebc183
Ali Piry <alipiry@icloud.com>
2019-01-28 20:28:33 +0330

    Update auto-generated files

---
+++ lib/tite.d.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+export declare function getTite(): void;
+export declare function getYear(): string;
+export declare function getMonth(): string;
+export declare function getDay(): string;
+export declare function getWeekDay(): string;
+export declare function getTime(): string;

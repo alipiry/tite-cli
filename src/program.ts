68c341adf2c344fa0f7f13224fd45e9af9b1e5ee
Ali Piry <alipiry@icloud.com>
2019-01-28 20:26:55 +0330

    Update options

--- src/program.ts
+++ src/program.ts
@@ -1,17 +1,29 @@
 #!/usr/bin/env node
 
 import program from 'commander';
+import {
+	getYear,
+	getMonth,
+	getDay,
+	getWeekDay,
+	getTime
+} from './tite';
+import chalk from 'chalk';
 
-export function options() {
+export function options(): void {
 	program
-		.version('1.1.0')
+		.version('1.3.0')
 		.description('Time & Date getter')
 		.option('-y, --year', 'Show year')
 		.option('-m, --month', 'Show month')
 		.option('-d, --day', 'Show day')
+		.option('-w, --weekday', 'Show week day')
+		.option('-t, --time', 'Show time')
 		.parse(process.argv);
 
-	if (!process.argv.slice(2).length) {
-			program.outputHelp();
-	}
+	if (program.year) console.log(chalk.bold.yellow(getYear()));
+	if (program.month) console.log(chalk.bold.yellow(getMonth()));
+	if (program.day) console.log(chalk.bold.yellow(getDay()));
+	if (program.weekday) console.log(chalk.bold.yellow(getWeekDay()));
+	if (program.time) console.log(chalk.bold.yellow(getTime()));
 }

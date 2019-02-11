ea7cb18a75c86186b755fa276ba672e758199f27
Ali Piry <alipiry@disroot.org>
2019-02-11 15:09:48 +0330

    Auto-generated files

--- lib/consts.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const gregorianMonths: Array<string>;
-export declare const jalaliMonths: Array<string>;
-export declare const gregorianWeekDays: Array<string>;
-export declare const jalaliWeekDays: Array<string>;
